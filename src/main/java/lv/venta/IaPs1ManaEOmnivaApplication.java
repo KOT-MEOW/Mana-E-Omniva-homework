d4af06a9c7f9720e19bfe03c4f8fe7b2433bab44
KOT-MEOW <QAZRFV123a>
2024-06-07 21:31:39 +0300

    Initial commit

---
+++ src/main/java/lv/venta/IaPs1ManaEOmnivaApplication.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaPs1ManaEOmnivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaPs1ManaEOmnivaApplication.class, args);
+	}
+
+}
